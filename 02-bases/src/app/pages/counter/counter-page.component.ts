e4769bfdb5fc2ba019eb0504c2426aab38c06c57
Karen Calderon <krcatari@umsa.bo>
2026-06-02 15:30:21 -0400

    añadiendo señales

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,12 +6,23 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
   counter = 10;
+  //usamos signal para crear un contador reactivo
+  counterSignal = signal(10);
+
+  constructor(){
+    setInterval(() => {
+      this.increaseByOne(1);
+      console.log('Tick');
+    }, 1000);
+  }
 
   increaseByOne(value: number) {
     this.counter += value;
+    this.counterSignal.update((current) => current + value);
   }
 
   reset() {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
