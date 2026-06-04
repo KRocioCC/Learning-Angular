d3fd3ee299826a862e8c6fac1310354a3c6fe3f7
Karen Calderon <krcatari@umsa.bo>
2026-06-04 08:25:17 -0400

    creando un nuevo componente y añadiendolo al navbar

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, computed, signal } from '@angular/core';
+// import { NgClass } from "@angular/common";
+
+interface Character {
+  id:number;
+  name: string;
+  power: number;
+}
+@Component({
+  templateUrl: './dragonball-page.component.html',
+  // imports: [NgClass],
+})
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', power: 9001},
+    {id: 2, name: 'Vegeta', power: 8000},
+    {id: 3, name: 'Gohan', power: 3000},
+  ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    }
+  });
+}
+
+
