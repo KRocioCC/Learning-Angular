cd7fe753b8ce181029e41800120614f7d8d2e3cf
Karen Calderon <krcatari@umsa.bo>
2026-06-04 09:31:47 -0400

    Dando funcionalidad a inputs del formulario

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -12,6 +12,9 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('gohan');
+  power = signal(0);
+
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001},
     {id: 2, name: 'Vegeta', power: 8000},
@@ -23,6 +26,23 @@ export class DragonballPageComponent {
       'text-danger': true,
     }
   });
+
+  addCharacter(): void {
+    if( this.power() <= 0 || !this.name() || !this.name() ){
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+    //queremos actualizar el valor de la señal
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
 }
-
-
